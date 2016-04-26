344f42b32297e7cf9fc7c1f0dea4b144198111a4
Alex Knauth <alexander@knauth.org>
2016-04-26 10:44:31 -0400

    add some simple tests for stream?

--- typed/racket/stream/tests/test-stream.rkt
+++ typed/racket/stream/tests/test-stream.rkt
@@ -18,6 +18,11 @@
 (define p
   (stream (begin (displayln "evaluating") 5)))
 
+(check-equal? (stream? s1212) #t)
+(check-equal? (stream? p) #t)
+(check-equal? (stream? (list 1 2 3)) #t)
+(check-equal? (stream? (vector 1 2 3)) #f)
+
 (let ([out (open-output-string)])
   (check-equal? (get-output-string out) "")
   (parameterize ([current-output-port out])
